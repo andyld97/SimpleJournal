b819f449125395714ddec33c7aa0156d71c595a9
andyld97 <andreasleopold97@hotmail.com>
2022-03-19 12:04:02 +0100

    feat: Added touch projects

---
+++ Helper Applications/TouchDeviceManager/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Device;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+
+namespace Touch_Device_Manager
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<DeviceWMI> devices = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            RefreshList();
+        }
+
+        public void RefreshList()
+        {
+            try
+            {
+                devices = DeviceWMI.GetPNPDevicesWithNames(new string[] { "touch screen", "touchscreen" });
+                ListDevices.ItemsSource = devices;
+                Title = $"Touch Device Manager - {devices.Count} Devices found!";
+
+                if (devices.Count > 0)
+                    ListDevices.SelectedIndex = devices.Count - 1;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+
+        private void ButtonDisable_Click(object sender, RoutedEventArgs e)
+        {
+            SetStateForSelectedIndex(ListDevices.SelectedIndex, false);
+        }
+
+        private void ButtonEnable_Click(object sender, RoutedEventArgs e)
+        {
+            SetStateForSelectedIndex(ListDevices.SelectedIndex, true);
+        }
+
+        private void SetStateForSelectedIndex(int index, bool state)
+        {
+            if (index < 0 || index > devices.Count - 1)
+            {
+                MessageBox.Show($"Please select a device first!", "No device selected!", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                devices[index].SetDeviceEnabled(state);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to set state {state} for device: {devices[index]}: {ex}", "Fail!", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            RefreshList();
+        }
+    }
+}
---
+++ Shared Libraries/Device/DeviceWMI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+
+namespace Device
+{
+    public class DeviceWMI
+    {
+        public string Name { get; set; }
+
+        public string ClassGUID { get; set; }
+
+        public List<string> HardwareID { get; set; }
+
+        public string DeviceID { get; set; }
+
+        public string PnpDeviceID { get; set; }
+
+        public DeviceWMI()
+        { }
+
+        public DeviceWMI(string name, string classGuid, List<string> hardwareID, string deviceID, string pnpDeviceID)
+        {
+            Name = name;
+            ClassGUID = classGuid;
+            HardwareID = hardwareID;
+            DeviceID = deviceID;
+            PnpDeviceID = pnpDeviceID;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}{Environment.NewLine}GUID: {ClassGUID}{Environment.NewLine}Device ID: {DeviceID}";
+        }
+
+        public static List<DeviceWMI> GetPNPDevicesWithNames(string[] names)
+        {
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity");
+            List<DeviceWMI> devices = new List<DeviceWMI>();
+
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                string name = queryObj["Name"]?.ToString();
+
+                if (!string.IsNullOrEmpty(name) && names.Any(p => name.ToLower().Contains(p.ToLower())))
+                {
+                    string guid = queryObj["ClassGuid"].ToString();
+                    var hardwareID = (string[])queryObj["HardwareID"];
+                    string deviceID = queryObj["DeviceID"].ToString();
+                    string pnpDeviceID = queryObj["PNPDeviceID"].ToString();
+
+                    devices.Add(new DeviceWMI(name, guid, hardwareID.ToList(), deviceID, pnpDeviceID));
+                }
+            }
+
+            return devices;
+        }
+    }
+}
