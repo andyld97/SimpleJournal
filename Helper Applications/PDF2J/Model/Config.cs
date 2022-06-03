69e73fc2761f7686c676599690b837ab45f41ebe
andyld97 <andreasleopold97@hotmail.com>
2022-06-03 11:34:08 +0200

    feat: Added url/port config options in PDF2J

--- Helper Applications/PDF2J/Model/Config.cs
+++ Helper Applications/PDF2J/Model/Config.cs
@@ -8,28 +8,44 @@ namespace PDF2J.Model
     public class Config
     {
         /// <summary>
+        /// The url where pdf2j is available under (must be specified fully like the default value)<br/>
+        /// To make this API publicly available there are two options:<br/><br/>
+        /// 1. Use localhost and an nginx (or any other) reverse proxy<br/>
+        /// 2. Use http://0.0.0.0 as url, then no reverse proxy is required!<br/><br/>
+        /// But don't forget to open <see cref="Port"/> in your firewall!<br/>
+        /// </summary>
+        [JsonPropertyName("url")]
+        public string WebAddress { get; set; } = "http://localhost";
+
+        /// <summary>
+        /// On this port the API will listen to incoming request (can be a value from 1-2^16, except ports which are occupied by default)
+        /// </summary>
+        [JsonPropertyName("port")]
+        public int Port { get; set; } = 5290;
+
+        /// <summary>
         /// The path where failed tickets get copied to, this
-        /// should be a seperate folder. A so called "Report" has
-        /// - a unique id (the guid of the ticket)
-        /// - exception stacktrace (log.txt)
+        /// should be a seperate folder. A so called "Report" has<br/><br/>
+        /// - a unique id (the guid of the ticket)<br/>
+        /// - exception stacktrace (log.txt)<br/><br/>
         /// and the document itself "doc.pdf"
         /// </summary>
         [JsonPropertyName("report_path")]
         public string ReportPath { get; set; }
 
         /// <summary>
-        /// Can be specified to notify a webhook (can be empty)
-        /// The url will be called if
-        /// - a conversation has finished successfully
-        /// - a converstaion has failed
-        /// The endpoint gets called with a message parameter containg the actual message (urlencoded)
+        /// Can be specified to notify a webhook (can be empty) <br/>
+        /// The url will be called if<br/><br/>
+        /// - a conversation has finished successfully<br/>
+        /// - a converstaion has failed<br/><br/>
+        /// The endpoint gets called with a message parameter containg the actual message (urlencoded)<br/>
         /// e.g. http://test.de/?message=xyz
         /// </summary>
         [JsonPropertyName("webhook_url")]
         public string WebHookUrl { get; set; }
 
         /// <summary>
-        /// If true failed tickets get saved to <see cref="ReportPath"/>
+        /// If true failed tickets get saved to <see cref="ReportPath"/> <br/>
         /// This settings also impacts the Webhook!
         /// </summary>
         [JsonPropertyName("report_failed_tickets")]
--- Helper Applications/PDF2J/Program.cs
+++ Helper Applications/PDF2J/Program.cs
@@ -38,8 +38,26 @@ namespace PDF2J
 
         public static void Main(string[] args)
         {
+            // Read config json (if any) [https://stackoverflow.com/a/28700387/6237448]
+            string configPath = System.IO.Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "config.json");
+            string preInitalizationLogMessage = string.Empty;
+            if (System.IO.File.Exists(configPath))
+            {
+                string configJson = System.IO.File.ReadAllText(configPath);
+                GlobalConfig = System.Text.Json.JsonSerializer.Deserialize<Config>(configJson);
+                preInitalizationLogMessage = "Successfully initalized config file!";
+            }
+            else
+            {
+                preInitalizationLogMessage = "No config file found ..."; 
+                GlobalConfig = new Config();
+            }
+
             var host = CreateHostBuilder(args).Build();
             logger = host.Services.GetService<ILogger<Program>>();
+
+            if (!string.IsNullOrEmpty(preInitalizationLogMessage))
+                logger.LogInformation(preInitalizationLogMessage);
 
             workingTimer = new System.Timers.Timer() { Interval = TimeSpan.FromSeconds(1).TotalMilliseconds };
             workingTimer.Elapsed += WorkingTimer_Elapsed;
@@ -48,21 +66,6 @@ namespace PDF2J
             cleanUPTimer = new System.Timers.Timer() { Interval = TimeSpan.FromHours(1).TotalMilliseconds };
             cleanUPTimer.Elapsed += CleanUPTimer_Elapsed;
             cleanUPTimer.Start();
-
-            // Read config json (if any)
-            // https://stackoverflow.com/a/28700387/6237448
-            string configPath  = System.IO.Path.Combine(PlatformServices.Default.Application.ApplicationBasePath, "config.json");
-            if (System.IO.File.Exists(configPath))
-            {
-                string configJson = System.IO.File.ReadAllText(configPath);
-                GlobalConfig = System.Text.Json.JsonSerializer.Deserialize<Config>(configJson);
-                logger.LogInformation("Successfully initalized config file!");
-            }
-            else
-            {
-                logger.LogInformation("No config file found ...");
-                GlobalConfig = new Config();
-            }
 
             host.Run();
         }
@@ -213,7 +216,7 @@ namespace PDF2J
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://localhost:5290");
+                    webBuilder.UseUrls($"{GlobalConfig.WebAddress}:{GlobalConfig.Port}");
                 });
     }
 }
