25866d76379ba5c4d849ea4f09ab5759e7050d73
andyld97 <andreasleopold97@hotmail.com>
2022-11-09 20:08:06 +0100

    fix: PackageReference

--- Shared Libraries/SimpleJournal.Common/Strings.cs
+++ Shared Libraries/SimpleJournal.Common/Strings.cs
@@ -4,8 +4,8 @@ namespace SimpleJournal.Common
 {
     public static class Strings
     {
-        public const string Version = "0.5.8.0";
-        public const string StoreVersion = "1.580.0.0";
+        public const string Version = "0.5.9.0";
+        public const string StoreVersion = "1.590.0.0";
         public const string Copyright = "Copyright © 2018-2022";
 
 #if RELEASE
