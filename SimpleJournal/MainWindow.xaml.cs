355a77c90b332ca80e7db5911fed5d8faa92d921
andyld97 <andreasleopold97@hotmail.com>
2023-12-21 01:00:32 +0100

    Fix

--- SimpleJournal/MainWindow.xaml.cs
+++ SimpleJournal/MainWindow.xaml.cs
@@ -153,8 +153,6 @@ namespace SimpleJournal
         {
             DrawingCanvas.HideSidebar += DrawingCanvas_HideSidebar;
             DrawingCanvas.PreventSelection += DrawingCanvas_PreventSelection;
-
-          UpdateHelper.  SearchForUpdates();
         }
 
         private static void DrawingCanvas_PreventSelection(object sender, EventArgs e)
