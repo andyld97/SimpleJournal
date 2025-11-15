d03baca2761d9916dec881c9e766d2fd7b4f945d
andyld97 <andreasleopold97@hotmail.com>
2025-11-15 12:26:08 +0100

    feat: PublishProfiles updated & fixed further warnings

---
+++ SimpleJournal/Helper/NativeMethods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SimpleJournal.Helper
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        internal static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        internal static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        internal static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        internal static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        internal static extern int SetWindowLong(IntPtr hwnd, int index, int value);
+
+        [DllImport("User32.dll", EntryPoint = "GetWindowLong")]
+        internal static extern Int32 GetWindowLongPtr(IntPtr hWnd, Int32 nIndex);
+
+        [DllImport("User32.dll", EntryPoint = "SetWindowLong")]
+        internal static extern Int32 SetWindowLongPtr(IntPtr hWnd, Int32 nIndex, Int32 dwNewLong);
+    }
+}
