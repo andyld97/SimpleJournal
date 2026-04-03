151fdbca414d3b725ab4780dffd65995452e44bb
andyld97 <andreasleopold97@hotmail.com>
2026-04-03 10:43:34 +0200

    feat: Updated version & nuget packges

--- Shared Libraries/SimpleJournal.Common/Strings.cs
+++ Shared Libraries/SimpleJournal.Common/Strings.cs
@@ -4,12 +4,12 @@ namespace SimpleJournal.Common
 {
     public static class Strings
     {
-        public const string Version = "0.7.1.0";
-        public const string StoreVersion = "1.710.0.0";
-        public const string Copyright = "Copyright © 2018-2025";
+        public const string Version = "0.7.2.0";
+        public const string StoreVersion = "1.720.0.0";
+        public const string Copyright = "Copyright © 2018-2026";
 
 #if UWPNEW || NORMAL
-        public static readonly DateTime ReleaseDate = new DateTime(2025, 12, 27, 12, 0, 0);
+        public static readonly DateTime ReleaseDate = new DateTime(2026, 04, 03, 10, 41, 0);
 #else 
         public static readonly DateTime ReleaseDate = DateTime.Now;
 #endif
